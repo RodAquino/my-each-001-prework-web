0900665a03ede0830bf998dc6bdd4398bc297d18
RodAquino <rodrigo_59@hotmail.com>
2015-12-10 22:43:05 +0000

    nitrous-backup at Thu Dec 10 22:43:05 UTC 2015

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,17 @@
-def my_each # put argument(s) here
-  # code here
+
+def first (number)
+  return number * 3
 end
+
+def second (number2)
+
+  return number2 + 10
+end
+first(3)
+second(4)
+
+def result(first,second)
+  puts first+second
+
+end
+result
